ce1394a3f4a3ace4775641066948d47aca1349fc
student <joycemumbiiwanjiku@gmail.com>
2023-05-06 22:48:46 +0300

    ch

--- ArrayObjects_Assgn.js
+++ ArrayObjects_Assgn.js
@@ -24,9 +24,46 @@ functionsumUnderagePersonAges(person)
 console.log(sumUnderagesPersonages(person));
 // Using JS functions and an array of numbers, return positive if an element
 //  within the array is positive, negative if an element is negative, else zero
-function number(Arr){
+function numbers(Arry){
+  for(i=o;i<Arry.lenght; i++){
+  if(Arry[i]<0){
+    return positive
+    }
+    else if (Arry[i]>0){
+      return "negative"
+    }
+    else{
+      return "zero"
+    }
+  }
+    }
+    console.log(numbers[-5,6,8,-43]);
   
-}
+
 // Given an array of objects, where each object represents an employee with 
 // an id, name, and salary property, write a function that returns a new array of employee
 //  objects sorted by their salary in ascending order.
+Employees=[
+  {
+    firstName:'Joyce',
+    LastName:'Mumbi',
+    age:43,
+    joinedDate:'December 15, 2010'
+  },
+  {
+    firstName:'Ann',
+    LastName:'Mary',
+    age:45,
+    joinedDate:'January 15,2012'
+  },
+  {
+    firstName:'Susan',
+    LastName:'Warimu',
+    age:21,
+    joinedDate:'March 15 2013'
+  }
+]
+Employees.sort((a,b)=>{
+  return a.age-b.age;
+})
+console.log({age});
